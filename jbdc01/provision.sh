66653d8e6fcef87ea4191e9b515c2413c601a942
David Sancho <dsanchor@redhat.com>
2016-05-03 17:38:01 +0200

    Commented apps deployment, so everything may run without errors if apps are not needed

--- jbdc01/provision.sh
+++ jbdc01/provision.sh
@@ -41,8 +41,8 @@ $JBOSS_HOME/bin/jboss-cli.sh --connect controller=$DOMAIN_CONTROLLER_HOST:9999 -
 # create demo system properties
 $JBOSS_HOME/bin/jboss-cli.sh --connect controller=$DOMAIN_CONTROLLER_HOST:9999 --user=$USER --password=$PASSWORD --file=$JBOSSSHARE_HOME/demo/scripts/create-sytem-properties.cli
 
-#deploy apps
-$JBOSS_HOME/bin/jboss-cli.sh --connect controller=$DOMAIN_CONTROLLER_HOST:9999 --user=$USER --password=$PASSWORD --file=$JBOSSSHARE_HOME/demo/scripts/deploy-apps.cli
+#deploy apps (uncomment and place demo apps under ./jbdc01/jbossshare/demo/apps)
+#$JBOSS_HOME/bin/jboss-cli.sh --connect controller=$DOMAIN_CONTROLLER_HOST:9999 --user=$USER --password=$PASSWORD --file=$JBOSSSHARE_HOME/demo/scripts/deploy-apps.cli
 
 #create activemq configuration
 $JBOSS_HOME/bin/jboss-cli.sh --connect controller=$DOMAIN_CONTROLLER_HOST:9999 --user=$USER --password=$PASSWORD --file=$JBOSSSHARE_HOME/demo/scripts/create-activemq-config.cli
